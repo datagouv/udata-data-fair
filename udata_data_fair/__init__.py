58990ddf8cce8dfd071a04d07a1fa9cd9799ec53
Alban Mouton <alban.mouton@gmail.com>
2020-03-16 15:42:02 +0100

    chore: apply 2t3 for udata2 compatibility

--- udata_data_fair/__init__.py
+++ udata_data_fair/__init__.py
@@ -4,7 +4,7 @@ data-fair
 
 Use DataFair embeds to preview data
 '''
-from __future__ import unicode_literals
+
 
 __version__ = '0.2.1'
 __description__ = 'Use DataFair embeds to preview data'
--- udata_data_fair/preview.py
+++ udata_data_fair/preview.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+
 
 from udata.core.dataset.preview import PreviewPlugin
 from udata.core.dataset.models import Resource
